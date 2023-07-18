5c554dcca30ab63408c41f8d53eeb27c4f9daf7c
shologin <mockingbird@mail.com>
2023-07-18 22:48:15 +0300

    fixed bug with lack of assets after build

--- src/levels/Level1.jsx
+++ src/levels/Level1.jsx
@@ -1,19 +1,29 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
+// import icons
+import deadpoolIcon from "../Assets/deadpool-icon.png";
+import hulkIcon from "../Assets/hulk-icon.png";
+import ironmanIcon from "../Assets/ironman-icon.png";
+import blackpantherIcon from "../Assets/blackpanther-icon.png";
+import captainamericaIcon from "../Assets/captainamerica-icon.png";
+import venomIcon from "../Assets/venom-icon.png";
+import wolverineIcon from "../Assets/wolverine-icon.png";
+import spidermanIcon from "../Assets/spiderman-icon.png";
+
 export default function Level1() {
   const navigate = useNavigate();
   useEffect(() => {
     const cards = document.querySelector(".cards");
     const colors = [
-      { background: 'url("src/Assets/deadpool-icon.png") maroon' },
-      { background: 'url("src/Assets/hulk-icon.png") lime' },
-      { background: 'url("src/Assets/ironman-icon.png") gold' },
-      { background: 'url("src/Assets/blackpanther-icon.png") purple' },
-      { background: 'url("src/Assets/captainamerica-icon.png") tomato' },
-      { background: 'url("src/Assets/venom-icon.png") dimgray' },
-      { background: 'url("src/Assets/wolverine-icon.png") khaki' },
-      { background: 'url("src/Assets/spiderman-icon.png") white' },
+      { background: `url(${deadpoolIcon}) maroon` },
+      { background: `url(${hulkIcon}) lime` },
+      { background: `url(${ironmanIcon}) gold` },
+      { background: `url(${blackpantherIcon}) purple` },
+      { background: `url(${captainamericaIcon}) tomato` },
+      { background: `url(${venomIcon}) dimgray` },
+      { background: `url(${wolverineIcon}) khaki` },
+      { background: `url(${spidermanIcon}) white` },
     ];
     const colorsPicker = [...colors, ...colors];
     const cardsCount = colorsPicker.length;
--- src/levels/Level3.jsx
+++ src/levels/Level3.jsx
@@ -2,23 +2,37 @@ import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import "../Styles/level3-style.css";
 
+// import icons
+import deadpoolIcon from "../Assets/deadpool-icon.png";
+import hulkIcon from "../Assets/hulk-icon.png";
+import ironmanIcon from "../Assets/ironman-icon.png";
+import blackpantherIcon from "../Assets/blackpanther-icon.png";
+import captainamericaIcon from "../Assets/captainamerica-icon.png";
+import venomIcon from "../Assets/venom-icon.png";
+import wolverineIcon from "../Assets/wolverine-icon.png";
+import spidermanIcon from "../Assets/spiderman-icon.png";
+import thanosIcon from "../Assets/thanos-icon.png";
+import hammerIcon from "../Assets/hammer-icon.png";
+import grootIcon from "../Assets/groot-icon.png";
+import antmanIcon from "../Assets/antman-icon.png";
+
 export default function Level3() {
   const navigate = useNavigate();
   useEffect(() => {
     const cards = document.querySelector(".cards-lvl3");
     const colors = [
-      { background: 'url("src/Assets/deadpool-icon.png") maroon' },
-      { background: 'url("src/Assets/hulk-icon.png") lime' },
-      { background: 'url("src/Assets/ironman-icon.png") gold' },
-      { background: 'url("src/Assets/blackpanther-icon.png") purple' },
-      { background: 'url("src/Assets/captainamerica-icon.png") tomato' },
-      { background: 'url("src/Assets/venom-icon.png") dimgray' },
-      { background: 'url("src/Assets/wolverine-icon.png") khaki' },
-      { background: 'url("src/Assets/spiderman-icon.png") white' },
-      { background: 'url("src/Assets/thanos-icon.png") indigo' },
-      { background: 'url("src/Assets/hammer-icon.png") silver' },
-      { background: 'url("src/Assets/groot-icon.png") olivedrab' },
-      { background: 'url("src/Assets/antman-icon.png") powderblue' },
+      { background: `url(${deadpoolIcon}) maroon` },
+      { background: `url(${hulkIcon}) lime` },
+      { background: `url(${ironmanIcon}) gold` },
+      { background: `url(${blackpantherIcon}) purple` },
+      { background: `url(${captainamericaIcon}) tomato` },
+      { background: `url(${venomIcon}) dimgray` },
+      { background: `url(${wolverineIcon}) khaki` },
+      { background: `url(${spidermanIcon}) white` },
+      { background: `url(${thanosIcon}) indigo` },
+      { background: `url(${hammerIcon}) silver` },
+      { background: `url(${grootIcon}) olivedrab` },
+      { background: `url(${antmanIcon}) powderblue` },
     ];
     const colorsPicker = [...colors, ...colors];
     const cardsCount = colorsPicker.length;
