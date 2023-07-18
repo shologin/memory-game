25fd71e460dabc6c37a0e0d6f9702b3a993e8c2f
shologin <mockingbird@mail.com>
2023-07-18 15:21:49 +0300

    finished game logic

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -3,10 +3,6 @@ import { useEffect } from "react";
 export default function Game() {
   useEffect(() => {
     const cards = document.querySelector(".cards");
-    // const colors = ["AliceBlue", "Chartreuse", "Aquamarine", "BlueViolet", "Brown", "BurlyWood", "CadetBlue", "Coral"];
-    // const colorsPicker = [...colors, ...colors];
-    // const cardsCount = colorsPicker.length;
-
     const colors = [
       { background: 'url("src/Assets/deadpool-icon.png") maroon' },
       { background: 'url("src/Assets/hulk-icon.png") lime' },
@@ -17,17 +13,8 @@ export default function Game() {
       { background: 'url("src/Assets/wolverine-icon.png") khaki' },
       { background: 'url("src/Assets/spiderman-icon.png") white' },
     ];
-
     const colorsPicker = [...colors, ...colors];
     const cardsCount = colorsPicker.length;
-
-    // const special = document.querySelector(".special");
-    // special.addEventListener("click", () => {
-    //   special.style.background = cardStyles[7].background;
-    //   special.style.backgroundSize = "80px";
-    //   special.style.backgroundRepeat = "no-repeat";
-    //   special.style.backgroundPosition = "center";
-    // });
 
     // Game state
     let revealedCount = 0;
@@ -38,10 +25,57 @@ export default function Game() {
     function createCard(color) {
       const card = document.createElement("div");
       card.classList.add("card");
-      card.style.background = color;
-      card.style.backgroundSize = "80px";
-      card.style.backgroundRepeat = "no-repeat";
-      card.style.backgroundPosition = "center";
+      card.setAttribute("data-color", color);
+      card.setAttribute("data-revealed", "false");
+
+      card.addEventListener("click", () => {
+        const revealed = card.getAttribute("data-revealed");
+        if (awaitingEndOfMove || revealed === "true" || card === activeCard) {
+          return;
+        }
+
+        card.style.background = color;
+        card.style.backgroundSize = "80px";
+        card.style.backgroundRepeat = "no-repeat";
+        card.style.backgroundPosition = "center";
+
+        if (!activeCard) {
+          activeCard = card;
+          return;
+        }
+
+        const colorToMatch = activeCard.getAttribute("data-color");
+        if (colorToMatch === color) {
+          activeCard.setAttribute("data-revealed", "true");
+          card.setAttribute("data-revealed", "true");
+          awaitingEndOfMove = false;
+          activeCard = null;
+          revealedCount += 2;
+
+          if (revealedCount === cardsCount) {
+            alert("You win! Refresh the page to play again");
+          }
+
+          return;
+        }
+
+        awaitingEndOfMove = true;
+
+        setTimeout(() => {
+          card.style.background = null;
+          card.style.backgroundSize = null;
+          card.style.backgroundRepeat = null;
+          card.style.backgroundPosition = null;
+
+          activeCard.style.background = null;
+          activeCard.style.backgroundSize = null;
+          activeCard.style.backgroundRepeat = null;
+          activeCard.style.backgroundPosition = null;
+
+          awaitingEndOfMove = false;
+          activeCard = null;
+        }, 1000);
+      });
 
       return card;
     }
