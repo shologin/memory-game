cb29df440b42b41a07fe19c2399dfd8e0c6fdb33
shologin <mockingbird@mail.com>
2023-08-03 22:58:47 +0300

    reverted to old settings

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/memory-game/",
+  base: "/memory-game",
   plugins: [react()],
 });
