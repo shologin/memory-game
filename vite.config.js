67f93743380bfef3b77ab21d7d42a7b74216ca39
shologin <mockingbird@mail.com>
2023-08-07 13:46:37 +0300

    reversed changes regards to gh-pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/memory-game",
+export default defineConfig({  
   plugins: [react()],
 });
